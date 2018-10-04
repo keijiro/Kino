dd6a97bf6a05bca8569e73e048ab6bd9165cf278
Keijiro Takahashi <keijiro@gmail.com>
2018-10-04 23:06:40 +0900

    Add sharpen filter

---
+++ Packages/jp.keijiro.kino.post-processing/Runtime/Sharpen.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace Kino.PostProcessing
+{
+    #region Effect settings
+
+    [System.Serializable]
+    [PostProcess(typeof(SharpenRenderer), PostProcessEvent.AfterStack, "Kino/Sharpen")]
+    public sealed class Sharpen : PostProcessEffectSettings
+    {
+        [Range(0, 1)] public FloatParameter strength = new FloatParameter { value = 0.5f };
+    }
+
+    #endregion
+
+    #region Effect renderer
+
+    sealed class SharpenRenderer : PostProcessEffectRenderer<Sharpen>
+    {
+        static class ShaderIDs
+        {
+            internal static readonly int Strength = Shader.PropertyToID("_Strength");
+        }
+
+        public override void Render(PostProcessRenderContext context)
+        {
+            var sheet = context.propertySheets.Get(Shader.Find("Hidden/Kino/PostProcessing/Sharpen"));
+            sheet.properties.SetFloat(ShaderIDs.Strength, settings.strength);
+
+            var cmd = context.command;
+            cmd.BeginSample("Sharpen");
+            cmd.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+            cmd.EndSample("Sharpen");
+        }
+    }
+
+    #endregion
+}
