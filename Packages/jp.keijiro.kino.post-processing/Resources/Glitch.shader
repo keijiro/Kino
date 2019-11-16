5576b99658bd6cf64cb959178db7e7f808100155
Keijiro Takahashi <keijiro@gmail.com>
2019-11-16 21:46:04 +0900

    Experimental implementation of block glitch effect

--- Packages/jp.keijiro.kino.post-processing/Resources/Glitch.shader
+++ Packages/jp.keijiro.kino.post-processing/Resources/Glitch.shader
@@ -30,6 +30,12 @@ Shader "Hidden/Kino/PostProcess/Glitch"
     }
 
     uint _Seed;
+
+    float _BlockStrength;
+    uint _BlockStride;
+    uint _BlockSeed1;
+    uint _BlockSeed2;
+
     float2 _Drift;
     float2 _Jitter;
     float2 _Jump;
@@ -37,10 +43,52 @@ Shader "Hidden/Kino/PostProcess/Glitch"
 
     TEXTURE2D_X(_InputTexture);
 
+    float FRandom(uint seed)
+    {
+        return GenerateHashedRandomFloat(seed);
+    }
+
     float4 Fragment(Varyings input) : SV_Target
     {
         UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+#if 1
+        uint block_size = 32;
+        uint columns = _ScreenSize.x / block_size;
 
+        // Block index
+        uint2 block_xy = input.texcoord * _ScreenSize.xy / block_size;
+        uint block = block_xy.y * columns + block_xy.x;
+
+        // Segment index
+        uint segment = block / _BlockStride;
+
+        // Per-block random number
+        float r1 = FRandom(block + _BlockSeed1);
+        float r3 = FRandom(block / 3 + _BlockSeed2);
+        uint seed = (r1 + r3) < 1 ? _BlockSeed1 : _BlockSeed2;
+        float rand = FRandom(segment + seed);
+
+        // Block damage (offsetting)
+        block += rand * 20000 * (rand < _BlockStrength);
+
+        // Screen space position reconstruction
+        uint2 ssp = uint2(block % columns, block / columns) * block_size;
+        ssp += (uint2)(input.texcoord * _ScreenSize.xy) % block_size;
+        ssp = (ssp + 0.5) % _ScreenSize.xy;
+
+        // Source color
+        float4 c = LOAD_TEXTURE2D_X(_InputTexture, ssp);
+
+        // Block damage (color mixing)
+        if (frac(rand * 1234) < _BlockStrength * 0.1)
+        {
+            float3 hsv = RgbToHsv(c.rgb);
+            hsv = hsv * float3(-1, 1, 0) + float3(0.5, 0, 0.9);
+            c.rgb = HsvToRgb(hsv);
+        }
+
+        return c;
+#else
         // Texture space position
         float tx = input.texcoord.x;
         float ty = input.texcoord.y;
@@ -68,6 +116,7 @@ Shader "Hidden/Kino/PostProcess/Glitch"
         float4 c2 = LOAD_TEXTURE2D_X(_InputTexture, uint2(sx2, sy));
 
         return float4(c1.r, c2.g, c1.b, c1.a);
+#endif
     }
 
     ENDHLSL
--- Packages/jp.keijiro.kino.post-processing/Runtime/Glitch.cs
+++ Packages/jp.keijiro.kino.post-processing/Runtime/Glitch.cs
@@ -7,6 +7,7 @@ namespace Kino.PostProcessing
     [System.Serializable, VolumeComponentMenu("Post-processing/Kino/Glitch")]
     public sealed class Glitch : CustomPostProcessVolumeComponent, IPostProcessComponent
     {
+        public ClampedFloatParameter block = new ClampedFloatParameter(0, 0, 1);
         public ClampedFloatParameter drift = new ClampedFloatParameter(0, 0, 1);
         public ClampedFloatParameter jitter = new ClampedFloatParameter(0, 0, 1);
         public ClampedFloatParameter jump = new ClampedFloatParameter(0, 0, 1);
@@ -17,8 +18,17 @@ namespace Kino.PostProcessing
         float _prevTime;
         float _jumpTime;
 
+        float _blockTime;
+        int _blockSeed1 = 71;
+        int _blockSeed2 = 113;
+        int _blockStride = 1;
+
         static class ShaderIDs
         {
+            internal static readonly int BlockSeed1 = Shader.PropertyToID("_BlockSeed1");
+            internal static readonly int BlockSeed2 = Shader.PropertyToID("_BlockSeed2");
+            internal static readonly int BlockStrength = Shader.PropertyToID("_BlockStrength");
+            internal static readonly int BlockStride = Shader.PropertyToID("_BlockStride");
             internal static readonly int Drift = Shader.PropertyToID("_Drift");
             internal static readonly int InputTexture = Shader.PropertyToID("_InputTexture");
             internal static readonly int Jitter = Shader.PropertyToID("_Jitter");
@@ -28,7 +38,7 @@ namespace Kino.PostProcessing
         }
 
         public bool IsActive() => _material != null &&
-            (drift.value > 0 || jitter.value > 0 || jump.value > 0 || shake.value > 0);
+            (block.value > 0 || drift.value > 0 || jitter.value > 0 || jump.value > 0 || shake.value > 0);
 
         public override CustomPostProcessInjectionPoint injectionPoint =>
             CustomPostProcessInjectionPoint.AfterPostProcess;
@@ -44,8 +54,22 @@ namespace Kino.PostProcessing
 
             // Update the time parameters.
             var time = Time.time;
-            _jumpTime += (time - _prevTime) * jump.value * 11.3f;
+            var delta = time - _prevTime;
+            _jumpTime += delta * jump.value * 11.3f;
             _prevTime = time;
+
+            // Block parameters
+            var block3 = block.value * block.value * block.value;
+
+            // Shuffle block parameters every 1/30 seconds.
+            _blockTime += delta * 30;
+            if (_blockTime > 1)
+            {
+                if (Random.value < 0.12f) _blockSeed1 += 251;
+                if (Random.value < 0.31f) _blockSeed2 += 373;
+                if (Random.value < 0.25f) _blockStride = Random.Range(1, 32);
+                _blockTime = 0;
+            }
 
             // Drift parameters (time, displacement)
             var vdrift = new Vector2(
@@ -65,6 +89,10 @@ namespace Kino.PostProcessing
 
             // Invoke the shader.
             _material.SetInt(ShaderIDs.Seed, (int)(time * 10000));
+            _material.SetFloat(ShaderIDs.BlockStrength, block3);
+            _material.SetInt(ShaderIDs.BlockStride, _blockStride);
+            _material.SetInt(ShaderIDs.BlockSeed1, _blockSeed1);
+            _material.SetInt(ShaderIDs.BlockSeed2, _blockSeed2);
             _material.SetVector(ShaderIDs.Drift, vdrift);
             _material.SetVector(ShaderIDs.Jitter, vjitter);
             _material.SetVector(ShaderIDs.Jump, vjump);
